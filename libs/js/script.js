f3c7f5f60760c3fa8b591fe184bc941831e131cb
Michael Snow <OriginalZenix@gmail.com>
2021-05-06 03:53:08 +0100

    Address Lookup now successfully calls data, provides error if no data Ocean Lookup in progress, one bug away from success.
    Signed-off-by: Michael Snow <OriginalZenix@gmail.com>

--- libs/js/script.js
+++ libs/js/script.js
@@ -6,61 +6,110 @@ $(window).on('load', function() {
     }
 });
 
-$('#btnCountryInfo').click(function() {
+$('#btnAddressSubmit').click(function() {
 
     $.ajax({
-        url: "libs/php/getCountryInfo.php",
+        url: "libs/php/getAddressLookup.php",
         type: 'POST',
         dataType: 'json',
         data: {
-            country: $('#selCountry').val(),
-            lang: $('#selLanguage').val()
+            lng: $('#addressLong').val(),
+            lat: $('#addressLat').val()
         },
         success: function(result) {
-
+            console.log("Basic success!");
             console.log(JSON.stringify(result));
-
+        
             if (result.status.name == "ok") {
-
-                document.getElementyById('resultsContainer').innerHTML = '<div name  id = "countryResult" name= "countryResult" class = "entry"><div>';
-                $('countryResult').append(result['data'][0]['continent']);
-                $('countryResult').append(result['data'][0]['capital']);
-                $('countryResult').append(result['data'][0]['languages']);
-                $('countryResult').append(result['data'][0]['population']);
-                $('countryResult').append(result['data'][0]['areaInSqKm']);           }
+                $('#streetNumber').empty();
+                $('#street').empty();
+                $('#postalCode').empty();
+                $('#countryCode').empty();
+                $('#streetNumber').append(result['data']['streetNumber']);
+                $('#street').append(result['data']['street']);       
+                $('#postalCode').append(result['data']['postalcode']);
+                $('#countryCode').append(result['data']['countryCode']);   
+            }
+            else if (result.status.name == "no data") {
+                $('#streetNumber').empty();
+                $('#street').empty();
+                $('#postalCode').empty();
+                $('#countryCode').empty();
+                $('#streetNumber').append('No Data Available for this lat/long set');
+                $('#street').append('No Data Available for this lat/long set');
+                $('#postalCode').append('No Data Available for this lat/long set');
+                $('#countryCode').append('No Data Available for this lat/long set');
+            }
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            // your error code
+            console.log("Latitude:"+$('#addressLong').val()+" Longitude:"+$('#addressLat').val());
+            console.log("Didn't work! Here's why...");
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log (errorThrown);
         }
     }); 
 
 });
 
 $('#btnOceanSubmit').click(function() {
-//Complete the PostalLookup
 $.ajax({
     url: "libs/php/getOceanInfo.php",
     type: 'POST',
     dataType: 'json',
     data: {
-        lat: $('#oceanLong').val(),
-        lang: $('#oceanLat').val()
+        lat: $('#oceanLat').val(),
+        long: $('#oceanLong').val()
     },
     success: function(result) {
 
         console.log(JSON.stringify(result));
         if (result.status.name == "ok") {
             console.log("OceanLookup Success!")
+            console.log(result['data']);
+            $('#oceanName').empty();
+            $('#oceanName').append(result['data']['name']);
+
+        }
+        else if (result.status.name == "no data") {
+            $('#oceanName').empty();
+            $('#oceanName').append('No Data Available');
         }
 
     },
     error: function(jqXHR, textStatus, errorThrown) {
-        // your error code
+        console.log("Didn't work! Here's why...");
+        console.log(jqXHR);
+        console.log(textStatus);
+        console.log (errorThrown);
+        console.log(result['data']);
     }
 }); 
 
 });
 
 $('#btnPostalSubmit').click(function() {
-    //Complete the PostalLookup
+    $.ajax({
+        url: "libs/php/getOceanInfo.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+            lat: $('#oceanLong').val(),
+            long: $('#oceanLat').val()
+        },
+        success: function(result) {
+    
+            console.log(JSON.stringify(result));
+            if (result.status.name == "ok") {
+                console.log("OceanLookup Success!")
+            }
+    
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("Didn't work! Here's why...");
+            console.log(jqXHR);
+            console.log(textStatus);
+            console.log (errorThrown);
+        }
+    })
 });
--- libs/php/getOceanInfo.php
+++ libs/php/getOceanInfo.php
@@ -7,7 +7,7 @@
 
 	$executionStartTime = microtime(true);
 
-	$url='http://api.geonames.org/countryInfoJSON?formatted=true&lang=' . $_REQUEST['lang'] . '&country=' . $_REQUEST['country'] . '&username=Vexxtraordinary&style=full';
+	$url='http://api.geonames.org/oceanJSON?formatted=true&lat=' . $_REQUEST['lat'] . '&lng=' . $_REQUEST['long'] . 'username=Vexxtraordinary';
 
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
@@ -19,13 +19,12 @@
 	curl_close($ch);
 
 	$decode = json_decode($result,true);	
-
 	$output['status']['code'] = "200";
 	$output['status']['name'] = "ok";
 	$output['status']['description'] = "success";
 	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
-	$output['data'] = $decode['geonames'];
-	
+	$output['data'] = $decode['ocean'];
+
 	header('Content-Type: application/json; charset=UTF-8');
 
 	echo json_encode($output); 
